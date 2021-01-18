ba158a017bbed3b918abf72959a74392f62b4485
RajathaDH <59072510+RajathaDH@users.noreply.github.com>
2021-01-18 11:08:10 +0530

    Added database connection

--- server.js
+++ server.js
@@ -3,8 +3,16 @@ if(process.env.NODE_ENV != 'production'){
 }
 
 const express = require('express');
+const mongoose  = require('mongoose');
+
+const DB_URI = process.env.MONGODB_URI;
+mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error(err));
 
 const app = express();
+
+app.set('view engine', 'ejs');
 
 const PORT = process.env.PORT || 5000;
 
