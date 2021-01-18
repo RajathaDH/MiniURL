74aa5336d2d3877c8b1426215df2dc1ffde37936
RajathaDH <59072510+RajathaDH@users.noreply.github.com>
2021-01-18 10:45:33 +0530

    Initialized web server

---
+++ server.js
@@ -0,0 +1,11 @@
+if(process.env.NODE_ENV != 'production'){
+    require('dotenv').config();
+}
+
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
